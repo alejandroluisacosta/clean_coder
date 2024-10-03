5f3cb2f6a03f678f02fac4ab299135a8a4a87736
Alejandro <alejandroluisacosta95@gmail.com>
2024-10-03 10:25:53 +0200

    add get characters with their image exercise

--- asynchronous_practice/rick_and_morty.js
+++ asynchronous_practice/rick_and_morty.js
@@ -1,6 +1,6 @@
 // GET A SINGLE CHARACTER
 
-let characters = [];
+// let characters = [];
 
 const getAllCharacters = async() => {
     try {
@@ -68,4 +68,68 @@ const displayCharacterEpisodes = async() => {
     await getCharacterEpisodes();
 }
 
-displayCharacterEpisodes();
+// displayCharacterEpisodes();
+
+// DISPLAY THE NAMES AND IMAGES OF ALL THE CHARACTERS THAT APPEAR ON A CERTAIN EPISODE
+const episode = 1;
+let characterUrls = [];
+let characters = [];
+
+// Fetch the list of urls for each characters from the episode
+const getCharacterUrls = async() => {
+    try {
+        const response = await fetch(`https://rickandmortyapi.com/api/episode/${episode}`);
+        if (response.ok) {
+            const jsonData = await response.json();
+            characterUrls = jsonData.characters;
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+// Fetch the name and the image url of each character from the list of urls
+const getCharactersInfo = async() => {
+    await getCharacterUrls();
+    for (let i = 0; i < characterUrls.length; i++) {
+        await getSingleCharacterInfo(characterUrls[i]);
+    }
+}
+
+// Fetch each individual character and push to characters array
+const getSingleCharacterInfo = async(url) => {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const jsonData = await response.json();
+            const characterName = jsonData.name;
+            const characterImage = jsonData.image;
+            characters.push({ name: characterName, image: characterImage });
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// Display list of characters
+const displayCharacters = async() => {
+    await getCharactersInfo();
+    const body = document.querySelector('body');
+    characters.forEach(character => {
+        const characterElementHTML = document.createElement('div');
+
+        const characterName = document.createElement('p');
+        characterName.innerHTML = character.name;
+
+        const characterImage = document.createElement('img');
+        characterImage.src = character.image;
+
+        characterElementHTML.appendChild(characterName);
+        characterElementHTML.appendChild(characterImage);
+        body.appendChild(characterElementHTML);
+    })
+}
+displayCharacters();
